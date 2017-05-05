1629d56bbe52438067b50760c8a94c84b32c34b1
Markus Fröschle <mfro@mubf.de>
2017-05-05 12:14:16 +0200

    make it work in all available EmuTOS resolutions

--- sources/blitter.c
+++ sources/blitter.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 #include <mint/osbind.h>
+#include <mint/linea.h>
 
 #include "portab.h"
 #include "blitter.h"
 #include "pattern.h"
 
-#define SCREEN_WIDTH    640
-#define SCREEN_HEIGHT   400
 
 #define NOP()   __asm__ __volatile__("nop");
 
 static volatile struct blitter_regs *blitter = (volatile struct blitter_regs *) 0xffff8a00;
+__LINEA *__aline;
+__FONT  **__fonts;
+short  (**__funcs) (void);
 
 static inline void blitter_start(void)
 {
@@ -28,14 +30,14 @@ static inline void blitter_start(void)
     __asm__ __volatile__(
         "start%=:                                   \t\n"
         "       lea     %[line_num],a0              \t\n"
-        "       bclr.b  %[hogbit],(a0)              \t\n"   // clear HOG bit - apparently wrong in Profibuch where the bit is set instead
-        "restart%=:                                 \t\n"   // contrary to the surrounding comments
+        "       bset.b  %[hogbit],(a0)              \t\n"
+        "restart%=:                                 \t\n"
         "       bset.b  %[busybit],(a0)             \t\n"
         "       nop                                 \t\n"
         "       jbne    restart%=                   \t\n"
         :
         : [line_num] "m" (blitter->line_num8), [hogbit] "i" (HOGBIT), [busybit] "i" (BUSYBIT)
-        : "a0", "cc"
+        : "a0", "memory", "cc"
     );
 }
 
@@ -80,48 +82,55 @@ static inline void blitter_set_fill_pattern(const UWORD pattern[], UWORD mask)
 }
 
 #define BITS_PER(a)     (sizeof(a) * 8)
-#define NUM_PLANES      8
-
+#define NUM_PLANES      __aline->_VPLANES
+#define SCREEN_WIDTH    (V_X_MAX + 1)
+#define SCREEN_HEIGHT   (V_Y_MAX + 1)
+#define SCR_WDWIDTH     ((SCREEN_WIDTH / BITS_PER(WORD)) * NUM_PLANES)
 
 void blit_area(WORD mode, void *src_addr, WORD src_x, WORD src_y, WORD dst_x, WORD dst_y, WORD w, WORD h, WORD hop)
 {
     int plane;
-    WORD x2 = dst_x + w - 1;
+    WORD x0 = dst_x;
+    WORD x1 = dst_x + w - 1;
+    WORD xcount = (x1 / BITS_PER(WORD) - x0 / BITS_PER(WORD));
     UWORD *scr = src_addr;
-    WORD start_word = dst_x / BITS_PER(WORD);
-    WORD end_word = x2 / BITS_PER(WORD);
-    WORD width_words = (end_word - start_word) + 1;
 
-    blitter->dst_xinc = NUM_PLANES * sizeof(WORD);
-    blitter->dst_yinc = (SCREEN_WIDTH / BITS_PER(WORD) - width_words + 1) * sizeof(UWORD) * NUM_PLANES;
-    blitter->src_xinc = NUM_PLANES * sizeof(WORD);
-    blitter->src_yinc = (dst_x + w) / 16 - dst_x / 16 + 1;
 
-    blitter->op = mode;
-    blitter->skew = 0; // dst_x & 15;
-    blitter->fxsr = 0;
-    blitter->nfsr = 0;
-    blitter->smudge = 0;
-    blitter->hop = hop;
-
-    /*
-     * determine endmasks
-     */
-    blitter->endmask1 = l_endmask[dst_x & 15];
-    blitter->endmask2 = ~0;
-    blitter->endmask3 = ~r_endmask[x2 & 15];
 
     for (plane = 0; plane < NUM_PLANES; plane++)
     {
-        blitter->x_count = width_words;
-        blitter->y_count = h;
+        blitter->op = mode;
+        blitter->skew = 0; // dst_x & 15;
+        blitter->fxsr = 0;
+        blitter->nfsr = 0;
+        blitter->smudge = 0;
+        blitter->hop = hop;
+
+        /*
+         * determine endmasks
+         */
+        blitter->endmask1 = l_endmask[x0 & 15];
+        blitter->endmask2 = ~0;
+        blitter->endmask3 = ~r_endmask[x1 & 15];
+        // printf("right endmask (r_endmask[%d]) : 0x%02x\r\n", x1 & 15, blitter->endmask3);
+        blitter->dst_xinc = NUM_PLANES * sizeof(WORD);      // offset to the next word in the same line and plane (in bytes)
+        blitter->dst_yinc = (SCR_WDWIDTH - xcount * NUM_PLANES) * 2;         // offset (in bytes) from the last word in current line to the first in next one
+
+        blitter->src_xinc = NUM_PLANES * sizeof(WORD);
+        blitter->src_yinc = (dst_x + w) / 16 - dst_x / 16;
 
         blitter->src_addr = scr;
 
-        blitter->dst_addr = scr +
-                            dst_y * (SCREEN_WIDTH / BITS_PER(UWORD) * NUM_PLANES) +
-                            start_word + plane;
+        blitter->x_count = xcount + 1;
+        blitter->y_count = h;
 
+        blitter->dst_addr = scr +                                       // start address
+                            dst_y * SCR_WDWIDTH +                       // + y * number of words/line
+                            x0 / BITS_PER(WORD) * NUM_PLANES +          // + x *
+                            plane;
+
+        //printf("blitter->xcount=%d, blitter_ycount=%d\r\nblitter->dst_xinc=%d,blitter->dst_yinc=%d\r\n,blitter->dst_addr=%p\r\n",
+        //       blitter->x_count, blitter->y_count, blitter->dst_xinc, blitter->dst_yinc, blitter->dst_addr);
         blitter_start();
     }
 }
@@ -193,14 +202,12 @@ void pump(void)
 void flicker(void)
 {
     int i;
-    void *start_addr = Physbase();
+    UWORD *start_addr = Physbase();
 
-    for (i = 0; i < 100; i++)
+    for (i = 1; i < 10; i++)
     {
-        blit_area(OP_ONE, start_addr, 0, 0, 1, 1, 400 - i, 400 - 2 * 16, HOP_ALL_ONE);
-        Vsync();
-        blit_area(OP_ZERO, start_addr, 0, 0, 1, 1, 400 - i, 400 - 2 * 16, HOP_ALL_ONE);
-        Vsync();
+        blit_area(OP_ONE, start_addr, 0, 0, 10, 10, V_X_MAX - 10 - 10, V_Y_MAX - 10 - 10, HOP_ALL_ONE);
+        blit_area(OP_ZERO, start_addr, 0, 0, 10, 10, V_X_MAX - 10 - 10, V_Y_MAX - 10 - 10, HOP_ALL_ONE);
     }
 }
 
@@ -268,6 +275,16 @@ void fill(void)
 
 int main(int argc, char *argv[])
 {
+
+    linea0();
+
+    (void) *__aline;
+    (void) *__fonts;
+    (void) *__funcs;
+
+    // printf("num_planes=%d\r\n", NUM_PLANES);
+    // Cconin();
+
     Supexec(blitter_init);
     Supexec(flicker);
     Supexec(pump);
